848a93089352207c0b309f8d9caaf2338c5962c9
Vishv Prakash Rai <vishvprakashrai@gmail.com>
2020-03-28 15:19:33 +0530

    Final Commit

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -5,6 +5,32 @@
  *  Time: 7:13 PM
  */
 package problem3.node;
-
 public class Node {
+    String name;
+    int roll;
+    Node next;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
